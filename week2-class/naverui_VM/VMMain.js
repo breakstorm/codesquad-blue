46c9f3420f6087539356c62497d9ac36253e9c21
김휘겸 <breakstorm00@gmail.com>
2017-03-20 21:38:14 +0900

    [study] Modify naverUI View-Model structure
     naverUI VIEW-MODEL 구조로 재설계
    
     Changes to be committed:
    	modified:   week2-class/naverui_VM/VMMain.js

--- week2-class/naverui_VM/VMMain.js
+++ week2-class/naverui_VM/VMMain.js
@@ -1,43 +1,50 @@
 function Model(){
-	this.currentIndex: 0;
-	this.subList: [];
-	this.unsubList: [];
-	this.jsonData:[
-	/*
-	 * title:
-	 * img:
-	 * article:
-	 * index:
-	 * subscription:
-	 */
-	];
+	this.currentIndex = 0;
+	this.subList= [];
+	this.unsubList= [];
+	this.jsonData=[
+	
+	//  * title:
+	//  * img:
+	//  * article:
+	//  * index:
+	//  * subscription:
+	 
+	]
 }
 var ModelObj = {
 	getAllSubList: function(){
 		return this.subList;
-	}
+	},
+
 	getAllUnsubList: function(){
 		return this.unsubList;
-	}
+	},
+
 	getSubLength: function(){
 		return this.subList.length;
-	}
+	},
+
 	getUnsubLength: function(){
 		return this.unsubList.length;
-	}
+	},
+
 	getSubList: function(index){
 		return this.jsonData[index];
-	}
+	},
+
 	getUnsubList: function(index){
 		return this.jsonData[index];
-	}
+	},
+
 	setjsonData: function(responsetext){
 		this.jsonData = responsetext;
 		for(var i = 0; i < responsetext.length; i++){
 			this.jsonData[i].index = i;
 			this.jsonData[i].subscription = "Y";
 		}
-	}
+	},
+
 	setList: function(){
 		var temp = this.jsonData;
 		for(var i = 0; temp.length; i++){
@@ -52,22 +59,22 @@ var HeadObj = {
 		this.leftrightButtonEvent();
 		this.newslistButtonEvent();
 		this.sublistButtonEvent();
-	}
+	},
 
 	//head페이지 렌더링
 	render: function(){
 
-	}
+	},
 
 	//페이징 버튼 이벤트
 	leftrightButtonEvent: function(){
 
-	}
+	},
 
 	//구독한 신문 보기 이동
 	newslistButtonEvent: function(){
 
-	}
+	},
 
 	//신문사 리스트 보기 이동
 	sublistButtonEvent: function(){
@@ -79,12 +86,12 @@ var NavObj = {
 	init: function(){
 		//해당 오브젝트에 있는 이벤트 선언
 		
-	}
+	},
 
 	//nav페이지 렌더링
 	renderNav: function(){
 
-	}
+	},
 
 	//news클릭 이벤트
 	newsClickEvent: function(){
@@ -97,27 +104,27 @@ var NavObj = {
 var ArticleObj = {
 	init: function(){
 		//해당 오브젝트에 있는 이벤트 선언
-	}
+	},
 
 	//Article페이징 렌더링
 	renderArticle: function(){
 
-	}
+	},
 
 	//구독 해지 버튼 이벤트
 	unsubButtonEvent: function(){
 
-	}
+	},
 
 	//newslistTemplate 불러오기
 	setNewslistTemplate: function(){
 
-	}
+	},
 
 	//newsTemplate 불러오기
 	setNewsTemplate: function(){
 
-	}
+	},
 
 	//newsCompanyTemplate 불러오기
 	setNewsCompanyTemplate: function(){
@@ -136,20 +143,35 @@ var NewsCompany = {
 var UtilObj = {
 	runAjax: function(url, fnc){
 		var oReq = new XMLHttpRequest();
-
+		oReq.addEventListener("load", fnc);
+		oReq.open("GET", url);
+		oReq.send();
 	}
 }
 
-document.addEventListener("DOMContentloaded", function(evt){
+function init(){
+	//ajax 데이터 가공
+	//선언된 객체 init함수 실행
+	debugger;
+	myHead.init();
+	myNav.init();
+	myArticle.init();
+	myCompany.init();
+}
+
+document.addEventListener("DOMContentLoaded", function(evt){
 	var url = "./data/newslist.json";
+	debugger;
 
 	//각 개체 선언 및 init() 호출
 	var myData = new Model();
 	Object.setPrototypeOf(myData, ModelObj);
 
-	var myHead = new HeadObj();
-	myHead.init();
-
-	//UtilObj.runAjax( ) 실행 
+	var myHead = Object.create(HeadObj);
+	var myNav = Object.create(NavObj);
+	var myArticle = Object.create(ArticleObj);
+	var myCompany = Object.create(NewsCompany);
+	
+	UtilObj.runAjax(url, myData.setjsonData);
 
 })
